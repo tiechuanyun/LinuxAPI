c9e09fb4553f8eb20c9ab1555d9fddf4ddaa9139
tiechuanyun <113876959+tiechuanyun@users.noreply.github.com>
2025-06-14 14:32:08 +0800

    README.mk

---
+++ README.mk
@@ -0,0 +1,4 @@
+1.阿里云云市场时间API接口
+https://market.aliyun.com/apimarket/detail/cmapi00065085?spm=5176.730005.result.2.5e15338ea6vmXV&innerSource=search
+2.高德天气API接口
+https://lbs.amap.com/api/webservice/guide/api/weatherinfo
